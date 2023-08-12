5fa0580676de9475f12d0bd8d1c78745978aaea7
Chiara Gizzi <chiaragizzi1@gmail.com>
2023-08-12 11:14:52 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Tabs from "./components/Tabs"
 
 function App() {
+  const [tabs, setTabs] = useState([
+    {
+      etiqueta: "Tab 1",
+      contenido: "Tab 1 content",
+      seleccionada: true,
+    },
+    {
+      etiqueta: "Tab 2",
+      contenido: "Tab 2 content",
+      seleccionada: false,
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs tabs={tabs} setTabs={setTabs}/>
     </div>
-  );
+  )
 }
 
 export default App;
